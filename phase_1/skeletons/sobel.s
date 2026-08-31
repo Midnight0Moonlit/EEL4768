69590f1b8c59c87b6ad7fb95a77729aa6c47777d
Midnight_Moonlit <gabrog@yahoo.com>
2026-08-31 11:18:53 -0400

    Added variables that point towards each needed matrices

--- phase_1/skeletons/sobel.s
+++ phase_1/skeletons/sobel.s
@@ -17,14 +17,35 @@ c:      .word 0, 0, 0
 .text
 .globl main
 
+# Extra notes to keep in mind:
+# Index = (Row x Total columns) + Col
+# Byte offset = Index x 4
+# Byte offset will help grab whatever number is in said matrix
+
+#when looping, we can add bytes in order to move on to the next set of area
+
 main:
 # Part 1: storing the 3x3 matrix
-        lw t0, 0(a0) # (0,0)
-        lw t1, 4(a0) # (0,1)
+# When storing matrices, start with lui for the starting memory
+# Then follow by ori so that the s values can direct towards the matrices
+# Start ori with 0, then add the number of bits needed based on that matrix when moving to the next
+
+        # Base address for Matrix A
+        lui s0, 0x10010 # memory layout
+        ori s0, t0 0x0000 # needed so it can point to Matrix A
+
+        # Base address for Matrix B
+        lui s1, 0x10010 # memory layout
+        ori s1, t0 0x0064 # needed so it can point to Matrix B, add 100 bytes based on the 5x5 previous matrix
+
+        # Base address for Matrix C
+        lui s0, 0x10010 # memory layout
+        ori s0, t0 0x0088 # needed so it can point to Matrix C, add 36 bytes based on the 3x3 previous matrix
 
 #Part 2: doing multiplication and such
 
 #PArt 3: Storing the matrix into c
 
 #Part 4: repeat
+done:
         ecall
